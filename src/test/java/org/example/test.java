2ebfdd6fb201054e20ae8a68755786e0f965b108
3301768269@qq.com <3301768269@qq.com>
2021-09-02 10:54:25 +0800

    项目初始化

--- src/test/java/org/example/test.java
+++ src/test/java/org/example/test.java
@@ -3,5 +3,7 @@ package org.example;
 public class test {
     public static void main(String[] args) {
         System.out.println("test......111!");
+        System.out.println("test......222");
+        System.out.println("test......333!");
     }
 }
